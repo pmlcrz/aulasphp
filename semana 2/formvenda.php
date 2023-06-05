de9d128594391b7681715f844c68097718185cd8
Pamela Cruz <117556345+pmlcrz@users.noreply.github.com>
2023-06-05 16:20:25 -0300

    Add files via upload

---
+++ semana 2/formvenda.php
@@ -0,0 +1,23 @@
+<html>
+<lang= pt-br>
+<head> 
+    <title>Vendas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+       
+</nav>
+    <center>
+
+<form action="recebeformvendas.php" method="post">
+    Produto <input type="text" name="produto"><br>
+    Quantidade <input type="number" name="quantidade"><br>
+    Preço <input type="text" name="preco"><br>
+    <input type ="submit" value="Cadastrar">
+</form>
+</center>
+
+</body>
+
+</html> 
---
+++ semana 2/recebeformvendas.php
@@ -0,0 +1,7 @@
+<?php
+$produto = $_POST['produto'];
+$quantidade = $_POST ['quantidade'];
+$preco = $_POST ['preco'];
+echo "produto: $produto <br>";
+echo "quantidade: $quantidade <br>";
+echo "preco: $preco <br>";
