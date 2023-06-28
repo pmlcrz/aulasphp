4a658c0e5b7bb48771a6380b395d6571da7ae1e6
Pamela Cruz <117556345+pmlcrz@users.noreply.github.com>
2023-06-28 16:06:51 -0300

    Add files via upload

--- semana 5/auladb2.sql
+++ semana 5/auladb2.sql
@@ -1,19 +1,7 @@
-/* sao os comentarios*/
-/*
-cria o banco de dados: create database databasename
-*/
-CREATE DATABASE dbSENAC;
-
-/* Ligar o banco de dados */
 use dbsenac;
 
-create table pessoa (
-  id int not null auto_increment,
-  nome varchar(56),
-  idade int(3),
-  salario float(10,2),
-  primary key(id)
-);  
+select * from pessoa;
+
 
 
 insert into pessoa 
@@ -23,15 +11,37 @@ values
 ('lua ', 21, 3000.45),
 ('miguel ', 25, 4000.45),
 ('gael ', 20, 2000.45),
+('Pietro ', 20, 2000.45),
 ('vitoria ', 35, 2000.45);
 
-select * from pessoa;
+
+use dbsenac;
+
+create table tabelaaluno (
+	id int not null auto_increment,
+    aluno varchar(50),
+    turma int(4),
+    mensalidade float(10,2), 
+    datainicio date,
+    primary key(id)
+);
 
 
-update pessoa set idade=18 where id=5;
-delete from pessoa where id=3;
-update pessoa set idade=15 where id=6;
+
+select * from tabelaaluno;
+
+insert into tabelaaluno
+(aluno, turma, mensalidade, datainicio)
+values
+('Cris', 3001, 456.87, '2023-02-01'),
+('Luana', 3002, 456.87, '2023-02-05'),
+('Pedro', 3001, 456.87, '2023-02-09'),
+('Paulo', 3002, 456.87, '2023-02-11');
 
 
-select * from pessoa 
-where nome like 'l%';
+
+select aluno,mensalidade from tabelaaluno where turma=3001;
+
+
+
+select * from tabelaaluno where id=2;
---
+++ semana 5/dbsistemaedu/listaralunos.php
@@ -0,0 +1,30 @@
+<?php
+include "conexao.php";
+$sql = "select * from tabelaaluno";
+$qry = mysqli_query($con,$sql);
+var_dump($qry);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Listagem de alunos</h2>
+        <hr>
+        <?php
+        while ($linha = mysqli_fetch_array($qry)){
+            echo $linha['id']."<br>";
+            echo $linha['aluno']."<br>";
+            echo $linha['turma']."<br>";
+            echo $linha['mensalidade']."<br>";
+            echo $linha['datainicio']."<br>";
+        }
+        ?>
+</body>
+</html>
