11929cbdbc7843d6aed4777618b8f6b4badfe029
Pamela Cruz <117556345+pmlcrz@users.noreply.github.com>
2023-07-12 15:48:48 -0300

    Add files via upload

---
+++ semana 7/formcadaluno2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de alunos</title>
+</head>
+<body>
+    <div class="container">
+        <form action="inseriraluno.php">
+            <div class="row">
+                <div class="col">
+<div class="mb-3">
+    
+    <form action="inseriraluno.php">
+        Aluno               <input type="text" name="aluno"><br>
+    </div>
+    </div>
+
+        Turma               <input type="text"  name="turma"><br>
+        </div>
+    </div>
+        Mensalidade         <input type="text"  name="mensalidade"><br> 
+        </div>
+    </div>
+        Data de início      <input type="date"  name="datainicio"><br>
+        </div>
+    </div>
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
